845507add4f7a01a90d6642fbf98e42e8b12b4a6
Matt Earl <git@matthewearl.com>
2023-12-17 19:24:24 +0000

    switch to simpler binary search method

--- optixlight/discrete.py
+++ optixlight/discrete.py
@@ -7,57 +7,45 @@ from . import q2bsp
 
 logger = logging.getLogger(__name__)
 
-_leaf_dtype = np.dtype([
+_entry_dtype = np.dtype([
+    ('p', np.float32),
     ('face_idx', np.uint32),
     ('tc', np.uint8, 2),
-    ('color', np.uint8, 3)
+    ('color', np.uint8, 3),
 ])
 
 
-def _write_subtree(leaves: np.ndarray, importance: np.ndarray,
-                   nodes: np.ndarray):
-    n = len(leaves)
-    assert n - 1 == len(nodes)
-    if n > 1:
-        n_left = (1 << int(np.ceil(np.log2(n)) - 1))
-        assert n > n_left > 0
+def _binary_search(cdf, v):
+    """Search for a value in a sorted array.
 
-        imp_left = np.sum(importance[:n_left])
-        imp_right = np.sum(importance[n_left:])
-        nodes[0] = imp_left / (imp_left + imp_right)
-        _write_subtree(leaves[:n_left], importance[:n_left], nodes[1:n_left])
-        _write_subtree(leaves[n_left:], importance[n_left:], nodes[n_left:])
+    Returns the largest `i` st `cdf[i] <= v`, or -1 if `v < cdf[i]` for all `i`.
+    """
 
-
-def random_sample(nodes):
-    leaf_num = 0
-    node_num = 0
-    num_leaves = len(nodes) + 1
-    n_left = (1 << int(np.ceil(np.log2(num_leaves)) - 1))
-
-    while num_leaves > 1:
-        if random.random() < nodes[node_num]:
-            node_num += 1
-            num_leaves = n_left
-            n_left = n_left >> 1
+    # invariant: cdf[lo] <= v < cdf[hi]
+    lo, hi = -1, len(cdf)
+    while lo != hi - 1:
+        mid = (lo + hi)//2
+        if v < cdf[mid]:
+            hi = mid
         else:
-            node_num += n_left
-            num_leaves -= n_left
-            leaf_num += n_left
-            while n_left >= num_leaves:
-                n_left = n_left >> 1
-    assert num_leaves == 1
-    return leaf_num
+            lo = mid
+    return lo
 
 
-def build_probability_tree(bsp: q2bsp.Q2Bsp):
-    logger.info('making leaves')
-    num_leaves = np.sum([
+def random_sample(cdf):
+    """Return entry number of a randomly selected entry."""
+    v = np.random.randint(0, 1<<32, dtype=np.uint32)
+    return _binary_search(cdf, v) + 1
+
+
+def build_source_cdf(bsp: q2bsp.Q2Bsp):
+    logger.info('making entries')
+    num_entries = np.sum([
         np.sum(np.any(face.extract_lightmap(0) > 0, axis=2))
         for face in bsp.faces
         if face.has_lightmap(0)
     ])
-    leaves = np.empty(num_leaves, dtype=_leaf_dtype)
+    entries = np.empty(num_entries, dtype=_entry_dtype)
     i = 0
     for face_idx, face in enumerate(f for f in bsp.faces if f.has_lightmap(0)):
         face_lm = face.extract_lightmap(0)
@@ -65,18 +53,28 @@ def build_probability_tree(bsp: q2bsp.Q2Bsp):
             for s in range(face_lm.shape[1]):
                 color = face_lm[t, s]
                 if np.any(color > 0):
-                    assert i < num_leaves
-                    leaves[i]['face_idx'] = face_idx
-                    leaves[i]['tc'] = (s, t)
-                    leaves[i]['color'] = color
+                    assert i < num_entries
+                    entries[i]['face_idx'] = face_idx
+                    entries[i]['tc'] = (s, t)
+                    entries[i]['color'] = color
                     i += 1
-    assert i == num_leaves
+    assert i == num_entries
 
     logger.info('making importance')
-    importance = leaves['color'].sum(dtype=np.uint16, axis=1)
+    importance = entries['color'].sum(dtype=np.uint16, axis=1)
 
-    logger.info('making nodes')
-    nodes = np.empty(len(leaves) - 1, dtype=np.float32)
-    _write_subtree(leaves, importance, nodes)
+    cdf = np.cumsum(importance, dtype=np.float64)
+    cdf = cdf[:-1] / cdf[-1]
+    cdf = np.ldexp(cdf, 32)
+    assert np.all(cdf <= 0xffffffff)
+    assert np.all(cdf >= 0)
+    cdf = cdf.astype(np.uint32)
 
-    return leaves, nodes, importance
+    entries['p'] = np.ldexp(
+        np.diff(
+            cdf.astype(np.uint64), prepend=0, append=(1<<32)
+        ).astype(np.float32),
+        -32
+    )
+
+    return entries, cdf, importance
